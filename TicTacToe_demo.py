b72d10c0f9c6f4751c7deb43f2a5756668f8d379
Ariel Beni <abenillouche07@gmail.com>
2025-03-23 15:10:03 +0200

    working version

---
+++ TicTacToe_demo.py
@@ -0,0 +1,8 @@
+import TicTacToeClient
+import TicTacToeServer
+#script to spin off server and two clients
+
+#run main of TicTacToeServer
+if __name__=='__main__':
+    TicTacToeServer.TicTacToeServer()
+    
